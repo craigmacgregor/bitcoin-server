78a862f9174fe040b924a3c0f57f38957f49bc1c
Craig <craig.b.macgregor@gmail.com>
2015-08-24 20:02:44 +1200

    bitcoin npm installed and tested from index

--- server/index.js
+++ server/index.js
@@ -5,3 +5,19 @@ require('babel-core/register');
 
 // Export the application
 exports = module.exports = require('./app');
+
+var bitcoin = require('bitcoin');
+var client = new bitcoin.Client({
+  host: 'localhost',
+  port: 8332,
+  user: 'bitcoinrpc',
+  pass: 'Ctda8mi3KF5FLymNmeA4m5Qo11Bz5TrUR47iGE4XqXq'
+});
+ 
+client.getDifficulty(function(err, difficulty) {
+  if (err) {
+    return console.error(err);
+  }
+ 
+  console.log('Difficulty: ' + difficulty);
+});
